729a6ed4493aa4a3c5657dcdbc130811a112e764
Surendra <170303105048@paruluniversity.ac.in>
2021-03-05 18:40:16 +0530

    Weather App

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import './App.css';
+import CityInput from './components/CityInput';
+import CityWeather from './components/CityWeather';
+function App() {
+  const [city, setCity] = useState("");
+  const [cityWeather, setCityWeather] = useState({});
+  const fetchCityWeather = ()=>{
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1dc861629d885f6ae8deb02ee6214529`
+    )
+    .then((response)=>response.json())  
+    .then((result)=>{
+      setCityWeather(result);
+      // console.log(cityWeather);  
+    })
+    .catch((error)=>{
+      console.log("error is", error);
+    })
+    ;
+  }
+  return (
+    <>
+      <CityInput city={city} setCity={setCity} fetchCityWeather={fetchCityWeather} />
+      <CityWeather cityWeather={cityWeather} /> 
+    </>
+  );
+}
+
+export default App;
